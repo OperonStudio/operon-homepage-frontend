369c4489ff0470fd168fb7e10375eb0dfde99906
devansh <devanshkg19@gmail.com>
2026-08-21 16:09:52 +0530

    fix: add mobile viewport meta tag

--- app/modules/root-document/index.tsx
+++ app/modules/root-document/index.tsx
@@ -26,6 +26,8 @@ export const RootDocument = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <HeadContent />
       </head>
       <body>
